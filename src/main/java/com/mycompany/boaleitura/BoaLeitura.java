80b31184ae88604d7090df3f0f5dee503e36dc3a
darknessblock <enzo.bondan@hotmail.com>
2023-09-22 15:25:00 -0400

    teste nova classe

--- src/main/java/com/mycompany/boaleitura/BoaLeitura.java
+++ src/main/java/com/mycompany/boaleitura/BoaLeitura.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.boaleitura;
-
-/**
- *
- * @author Edson
- */
 public class BoaLeitura {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(":3");
     }
 }
---
+++ src/main/java/com/mycompany/boaleitura/Livro.java
@@ -0,0 +1,16 @@
+package com.mycompany.boaleitura;
+public class Livro {
+
+    String title;
+    String autor;
+    String editora;
+    int qntd;
+
+    public Livro(String title, String autor, String editora, int qntd) {
+        this.title = title;
+        this.autor = autor;
+        this.editora = editora;
+        this.qntd = qntd;
+    }
+
+}
