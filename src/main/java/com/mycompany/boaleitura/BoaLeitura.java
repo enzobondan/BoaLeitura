b14425aa3aa83f0b2ec15ad582b64c30d1963f23
darknessblock <enzo.bondan@hotmail.com>
2023-10-02 20:29:09 -0400

    main corrigida

--- src/main/java/com/mycompany/boaleitura/BoaLeitura.java
+++ src/main/java/com/mycompany/boaleitura/BoaLeitura.java
@@ -1,14 +1,17 @@
 package com.mycompany.boaleitura;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BoaLeitura {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":3");
-        Object[] vendas;
-        Object[] estoque;
-        String choice = "";
+        ArrayList<Livro> estoque = new ArrayList<Livro>();
+        ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        ArrayList<Livro> vendas = new ArrayList<Livro>();
+        String choice;
         do {
         System.out.println("Prezado Usuário, qual opção deseja realizar?\n1 - Cadastrar Livro\n2 - Registrar uma Venda\n3 - Verificar histórico de vendas do dia(o array de vendas)\n4 - Verificar Estoque(array estoque)\n5 - Alugar um livro para um Cliente\n6 - Gravar Vendas na máquina\n7 - Encerrar o Sistema");
         choice = scan.nextLine();
@@ -21,26 +24,21 @@ public class BoaLeitura {
               String author = scan.nextLine();
               System.out.println("Insira a Editora da edição do livro: ");
               String editora = scan.nextLine();
-              System.out.println(String.format("Insira por fim quantos livros do título '%s' Deseja adicionar em estoque", bookName));
-              int qntd = scan.nextInt();
-              if (estoque.length == 0) {
+              if (estoque.isEmpty()) {
                 id = 1;
               } else {
-                for (int i = 0; i < estoque.length; i++) {
-                  if (estoque[i].title.equals(bookName)) {
-                    id = estoque[i].id;
+                id = estoque.size()+ 1;
                   }
-                }
-              }
-              
-                return id;
-              }
-            break;
-        
+              Livro novoLivro = new Livro(bookName,id,author,editora);
+              estoque.add(novoLivro);
+              Livro livro = estoque.get(0);
+              System.out.println("Título do Livro inserido: " + livro.getTitle());
+          break; 
           default:
-            break;
+            System.out.println("Insira um valor válido");
+          break;
         }
-      } while (!choice.equals("7"));
+      } while (!"7".equals(choice));
 
 
     }
--- src/main/java/com/mycompany/boaleitura/Livro.java
+++ src/main/java/com/mycompany/boaleitura/Livro.java
@@ -5,16 +5,61 @@ public class Livro {
     private int id;
     public String autor;
     public String editora;
-    private int qntd;
     protected Boolean Alugado;
     protected Object clienteDevedor;
 
-    public Livro(String title, int id, String autor, String editora, int qntd) {
-        this.id = id;
+    public Livro(String title, int id, String autor, String editora) {
         this.title = title;
         this.autor = autor;
         this.editora = editora;
-        this.qntd = qntd;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public Boolean getAlugado() {
+        return Alugado;
+    }
+
+    public void setAlugado(Boolean Alugado) {
+        this.Alugado = Alugado;
+    }
+
+    public Object getClienteDevedor() {
+        return clienteDevedor;
+    }
+
+    public void setClienteDevedor(Object clienteDevedor) {
+        this.clienteDevedor = clienteDevedor;
     }
 
 }
