3519dfeef21602d37af8583f65b9dbe47857bba3
darknessblock <enzo.bondan@hotmail.com>
2023-10-02 20:30:15 -0400

    livro sem qntd

--- src/main/java/com/mycompany/boaleitura/Livro.java
+++ src/main/java/com/mycompany/boaleitura/Livro.java
@@ -13,7 +13,6 @@ public class Livro {
         this.autor = autor;
         this.editora = editora;
     }
-
     public String getTitle() {
         return title;
     }
@@ -44,62 +43,6 @@ public class Livro {
 
     public void setEditora(String editora) {
         this.editora = editora;
-    }
-
-    public Boolean getAlugado() {
-        return Alugado;
-    }
-
-    public void setAlugado(Boolean Alugado) {
-        this.Alugado = Alugado;
-    }
-
-    public Object getClienteDevedor() {
-        return clienteDevedor;
-    }
-
-    public void setClienteDevedor(Object clienteDevedor) {
-        this.clienteDevedor = clienteDevedor;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public String getEditora() {
-        return editora;
-    }
-
-    public void setEditora(String editora) {
-        this.editora = editora;
-    }
-
-    public int getQntd() {
-        return qntd;
-    }
-
-    public void setQntd(int qntd) {
-        this.qntd = qntd;
     }
 
     public Boolean getAlugado() {
